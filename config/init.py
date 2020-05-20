c401534716e3c6b5b3a27659760a151842005088
denis-yu <denis.yu@suryani.cn>
2020-05-20 15:41:07 +0800

    unitest passed

---
+++ config/init.py
@@ -0,0 +1,4 @@
+
+screenPath = "/Users/denisyu/workplace/test/Screenshots/Medicare/"
+reportPath = "/Users/denisyu/workplace/test/NewWebTest/report/"
+dataPath= "/Users/denisyu/workplace/test/NewWebTest/data/"
